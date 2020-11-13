67b2b30fe2ae1ed836d24639b0a5d7d9896d6a00
Enea Mancellari <emancellari@gmail.com>
2020-11-13 18:03:09 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int sum (int x, int y, int z=0, int w=0){
+
+  return (x+y+z+w);
+}
+
+int main(){
+ cout<< sum(1,2)<<endl;
+ cout<< sum(1,2,3)<<endl;
+ cout<< sum(1,2,4,5)<<endl;
+
+  return 0;
+}
